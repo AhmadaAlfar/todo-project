603c18aa148bd2b0fcbd2de3e749bd151628b97f
AhmadaAlfar <ahm.ataalfar@gmail.com>
2023-04-04 11:37:51 +0000

    js

---
+++ app.js
@@ -0,0 +1,22 @@
+let user1 = prompt("enter her/his name");
+console.log('my name is >>>>>', user1);
+
+let user2 = prompt("enter her/his gender");
+if(user2 == 'male' ){
+
+    console.log('my gender is ',user2);
+}
+  else if (user2 == 'female'){
+    
+ console.log('my gender is',user2)}
+else {
+console.log ('the gender is incorrect');}
+
+let user3= prompt("enter her/his age");
+if(user3 > 0 ){
+
+console.log('my age is >>>' , user3);}
+ else {
+   alert("you enter incorrect age");}
+
+alert("welcome MR/MS" );
