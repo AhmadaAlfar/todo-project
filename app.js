9c2c88dd8ce4daf9afb27aac9fd972128df7b39e
AhmadaAlfar <ahm.ataalfar@gmail.com>
2023-04-05 15:51:06 +0000

    js2

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
+
 let userName = prompt("enter her/his name");
-
-
 let gender = prompt("enter her/his gender");
-if (gender != 'male'&&'female') {
+if (gender != 'male' && 'female') {
   alert("the gender is incorrect")
-
 }
-
-
-let age= prompt("enter her/his age");
-if (age <= 0 ){
+let age = prompt("enter her/his age");
+if (age <= 0) {
   alert(" your age incoorect ");
 
 }
-
-
-
-   
-let askuser = prompt ("did u wants to skip the welcoming message.");
-if(askuser == 'yes'){
- 
-}
-else if (askuser =='no' && gender=='male'){
-  alert('welcome MR' + '  '+userName);
+let askuser = prompt("did u wants to skip the welcoming message.");
+if (askuser == 'yes') {
 
 }
-else if (askuser =='no' && gender=='female'){
-  alert('welcome MS' + '  '+userName);
+else if (askuser == 'no' && gender == 'male') {
+  alert('welcome MR' + '  ' + userName);
+
+}
+else if (askuser == 'no' && gender == 'female') {
+  alert('welcome MS' + '  ' + userName);
 }
 else {
   alert('welcome');
 }
+
+
+
+let answers =[Answer1,Answer2,Answer3];
+let Answer1 = prompt("are you ok");
+let Answer2 = prompt("do you like js until now");
+let Answer3 = prompt("did you practise code as alone ");
+
+for(let i = 0;  i<answers.length; i++){
+
+  console.log(answers[i]);
+}
+
 
 
