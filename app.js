13a8c6b5d011997d1cba98082c10a81d7814c272
AhmadaAlfar <ahm.ataalfar@gmail.com>
2023-04-06 21:04:24 +0000

    js2 done

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 
-let userName = prompt("enter her/his name");
-let gender = prompt("enter her/his gender");
-if (gender != 'male' && 'female') {
-  alert("the gender is incorrect")
-}
-let age = prompt("enter her/his age");
-if (age <= 0) {
-  alert(" your age incoorect ");
+// let userName = prompt("enter her/his name");
+// let gender = prompt("enter her/his gender");
+// if (gender != 'male' && 'female') {
+//   alert("the gender is incorrect")
+// }
+// let age = prompt("enter her/his age");
+// if (age <= 0) {
+//   alert(" your age incoorect ");
 
-}
-let askuser = prompt("did u wants to skip the welcoming message.");
-if (askuser == 'yes') {
+// }
+// let askuser = prompt("did u wants to skip the welcoming message.");
+// if (askuser == 'yes') {
 
-}
-else if (askuser == 'no' && gender == 'male') {
-  alert('welcome MR' + '  ' + userName);
+// }
+// else if (askuser == 'no' && gender == 'male') {
+//   alert('welcome MR' + '  ' + userName);
 
-}
-else if (askuser == 'no' && gender == 'female') {
-  alert('welcome MS' + '  ' + userName);
-}
-else {
-  alert('welcome');
-}
+// }
+// else if (askuser == 'no' && gender == 'female') {
+//   alert('welcome MS' + '  ' + userName);
+// }
+// else {
+//   alert('welcome');
+// }
+
+/*-------------------------lap 06 -----------------------------*/
 
 
+function answerQuestion(answer){
+
+  if (answer == ""){
+    return "invalid string";
+  }
+  return answer;
+ 
+ }
+ let userAsk = prompt("do you practise js");
+ let userAsk2 = prompt("do you feel this course improve your skills");
+ let userAsk3 = prompt("do you spent 5 hours every day to study");
+
+ console.log('the answer 1 '+ answerQuestion(userAsk));
+ console.log('the answer 2 '+ answerQuestion(userAsk2));
+ console.log('the answer 3 '+ answerQuestion(userAsk3));
 
 
-let Answer1 = prompt("are you ok");
-let Answer2 = prompt("do you like js until now");
-let Answer3 = prompt("did you practise code as alone ");
-let answers =[Answer1,Answer2,Answer3];
-for(let i = 0;  i<answers.length; i++){
-
-  console.log(answers[i]);
-}
-
+ 
 
 
