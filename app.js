bfd03e6252251c9b56d3816100263048e3b8acd1
AhmadaAlfar <ahm.ataalfar@gmail.com>
2023-04-09 22:48:16 +0000

    js 2

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 
-// let userName = prompt("enter her/his name");
-// let gender = prompt("enter her/his gender");
-// if (gender != 'male' && 'female') {
-//   alert("the gender is incorrect")
-// }
-// let age = prompt("enter her/his age");
-// if (age <= 0) {
-//   alert(" your age incoorect ");
+let userName = prompt("enter her/his name");
+let gender = prompt("enter her/his gender");
+if (gender != 'male' && 'female') {
+  alert("the gender is incorrect")
+}
+let age = prompt("enter her/his age");
+if (age <= 0) {
+  alert(" your age incoorect ");
 
-// }
-// let askuser = prompt("did u wants to skip the welcoming message.");
-// if (askuser == 'yes') {
+}
+let askuser = prompt("did u wants to skip the welcoming message.");
+if (askuser == 'yes') {
 
-// }
-// else if (askuser == 'no' && gender == 'male') {
-//   alert('welcome MR' + '  ' + userName);
+}
+else if (askuser == 'no' && gender == 'male') {
+  alert('welcome MR' + '  ' + userName);
 
-// }
-// else if (askuser == 'no' && gender == 'female') {
-//   alert('welcome MS' + '  ' + userName);
-// }
-// else {
-//   alert('welcome');
-// }
+}
+else if (askuser == 'no' && gender == 'female') {
+  alert('welcome MS' + '  ' + userName);
+}
+else {
+  alert('welcome');
+}
 
 /*-------------------------lap 06 -----------------------------*/
 
